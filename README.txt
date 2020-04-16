9dfc975df75c06e198d8226edf45ae2067821036
Andrew Leifer <leifer@princeton.edu>
2020-04-15 21:15:54 -0400

    readme. Read from A01 too

--- README.txt
+++ README.txt
@@ -1,3 +1,6 @@
+An extremely simple script to read a voltage from a labjack A to D.
+This is designed to read the voltage from an Aalborg Mass Flow Controller.
+
 Download the exodriver for macos:
 https://labjack.com/sites/default/files/Exodriver_NativeUSB_Setup.zip
 and install.
--- read_labjack.py
+++ read_labjack.py
@@ -6,6 +6,7 @@ d = u3.U3() #find the labjack and attach
 #Set up the addresses of the various pins
 # Following example: https://labjack.com/support/software/examples/ud/labjackpython/modbus
 AIN0_REGISTER = 0
+AIN1_REGISTER = 2
 print('Found a labjack!')
 
 
@@ -17,7 +18,8 @@ import time # import timing library
 
 sampling_freq = 10 #Hz (approximate)
 
-Vin_array = np.array([])
+Vin0_array = np.array([])
+Vin1_array = np.array([])
 timestamp_array = np.array([])
 
 #setup file for writing:
@@ -30,19 +32,22 @@ print("Hit Control-c at any point to quit")
 try:
     while True:
 
-        Vin = d.readRegister(AIN0_REGISTER)
-        Vin_array = np.append(Vin_array, Vin)
+        Vin0 = d.readRegister(AIN0_REGISTER)
+        Vin0_array = np.append(Vin0_array, Vin0)
+
+        Vin1 = d.readRegister(AIN1_REGISTER)
+        Vin1_array = np.append(Vin1_array, Vin1)
 
         timestamp = time.time() #seconds since 1970
         timestamp_array = np.append(timestamp_array, timestamp)
 
-        print("Vin = %.3f, Seconds Since 1970 = %.1f " % (Vin, timestamp))
+        print("AIN0 = %.3f V, AIN1 = %.3f V, Seconds Since 1970 = %.1f s" % (Vin0, Vin1, timestamp))
 
         # Wait one interval of the sampling rate
         time.sleep(1 / sampling_freq)
 
 except KeyboardInterrupt:
     print("writing file " + filename)
-    np.savetxt(filename, np.transpose([Vin_array, timestamp_array]), delimiter=',')
+    np.savetxt(filename, np.transpose([Vin0_array, Vin1_array, timestamp_array]), delimiter=',')
     print("written!")
     pass
